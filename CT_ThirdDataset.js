6429e39a5faf4391f4e62e24d77a52b6eaac003d
charliecaiftaa <charlie.cai@g.feitianacademy.org>
2021-03-04 13:13:44 -0500

    123

--- CT_ThirdDataset.js
+++ CT_ThirdDataset.js
@@ -15,7 +15,7 @@ let First_Data ="SELECT * FROM dtrends.covid_19_OGC WHERE Date = ?;"
 let Sec_Data = "SELECT * FROM dtrends.covid_19_JH WHERE Date = ?;"
 
 con.query(Sec_Data,Date_n,function (err, Sec_Data) {
-   // console.log(Sec_Data);
+    // console.log(Sec_Data);
     con.query(First_Data,Date_n,function (err, First_Data) {
         // console.log(Sec_Data);
 
@@ -61,31 +61,31 @@ con.query(Sec_Data,Date_n,function (err, Sec_Data) {
         //
         //
         // }
-            for(let k = 0; k < First_Data.length; k++){
-                let Only_Data_a = "SELECT LayerName FROM dtrends.covid_19_JH WHERE LayerName = ?;"
-                con.query(Only_Data_a, First_Data[k].LayerName, function (err, result) {
-                    if(result == ""){
-                        let Third_data_OGC = "INSERT INTO dtrends.covid_19_Final(Date, LayerName, LayerType, FirstLayer, DisplayName, CaseNum, DeathNum, RecovNum," +
-                            " ActiveNum, Latitude, Longitude,CityName, StateName, CountryName, ContinentName, Color_Confirmed, Color_Death, Color_Recovered) " +
-                            "VALUES (?,?,'H_PKLayer','Corona_Virus',?,?,?,?,?,?,?,?,?,?,?,'rgb(220,0,0) rgb(220,0,0) rgb(220,0,0)','rgb(0,0,0) rgb(0,0,0) rgb(0,0,0)','rgb(124,252,0) rgb(124,252,0) rgb(124,252,0)'); ";
-                        con.query(Third_data_OGC,[First_Data[k].Date, First_Data[k].LayerName, First_Data[k].DisplayName, First_Data[k].CaseNum,
-                            First_Data[k].DeathNum, First_Data[k].RecovNum, First_Data[k].ActiveNum, First_Data[k].Latitude, First_Data[k].Longitude,
-                            First_Data[k].CityName, First_Data[k].StateName,First_Data[k].CountryName,First_Data[k].ContineNtname] , function (err, result) {
-                            if (err) {
-                                console.log(err);
-                            }else{
-                                // if(k === First_Data.length -2){
-                                //     console.log("123456")
-                                // }
-                            }
-                        });
-                    }else{
-                        console.log("1");
+        for(let k = 0; k < First_Data.length; k++){
+            let Only_Data_a = "SELECT LayerName FROM dtrends.covid_19_JH WHERE LayerName = ?;"
+            con.query(Only_Data_a, First_Data[k].LayerName, function (err, result) {
+                if(result == ""){
+                    let Third_data_OGC = "INSERT INTO dtrends.covid_19_Final(Date, LayerName, LayerType, FirstLayer, DisplayName, CaseNum, DeathNum, RecovNum," +
+                        " ActiveNum, Latitude, Longitude,CityName, StateName, CountryName, ContinentName, Color_Confirmed, Color_Death, Color_Recovered) " +
+                        "VALUES (?,?,'H_PKLayer','Corona_Virus',?,?,?,?,?,?,?,?,?,?,?,'rgb(220,0,0) rgb(220,0,0) rgb(220,0,0)','rgb(0,0,0) rgb(0,0,0) rgb(0,0,0)','rgb(124,252,0) rgb(124,252,0) rgb(124,252,0)'); ";
+                    con.query(Third_data_OGC,[First_Data[k].Date, First_Data[k].LayerName, First_Data[k].DisplayName, First_Data[k].CaseNum,
+                        First_Data[k].DeathNum, First_Data[k].RecovNum, First_Data[k].ActiveNum, First_Data[k].Latitude, First_Data[k].Longitude,
+                        First_Data[k].CityName, First_Data[k].StateName,First_Data[k].CountryName,First_Data[k].ContineNtname] , function (err, result) {
+                        if (err) {
+                            console.log(err);
+                        }else{
+                            // if(k === First_Data.length -2){
+                            //     console.log("123456")
+                            // }
+                        }
+                    });
+                }else{
+                    console.log("1");
 
-                    }
+                }
 
-                });
-            }
+            });
+        }
 
     });
 
